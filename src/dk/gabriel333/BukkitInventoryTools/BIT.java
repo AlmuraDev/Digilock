32bd30433ac64ddffe1d44af7e9ac934407b1b2e
balr0g <@null>
2012-02-24 21:50:55 -0500

    derp x2

--- src/dk/gabriel333/BukkitInventoryTools/BIT.java
+++ src/dk/gabriel333/BukkitInventoryTools/BIT.java
@@ -80,7 +80,7 @@ public class BIT extends JavaPlugin {
 			setupMobArena();
 			setupJail();
 			setupWar();
-                        if (!BITConfig.SBP_EnableEconomy)
+                        if (BITConfig.SBP_EnableEconomy)
         			useEconomy = setupEconomy();
 			li = new BITBackpackLanguageInterface(BITBackpack.loadLanguage());
 			// Load BITBackpack
