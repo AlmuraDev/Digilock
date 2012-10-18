beeb40f7c31ebf9de49593bb71558d518051710f
mcsnetworks <mike@mcsnetworks.com>
2012-10-18 09:29:58 -0500

    Fix the screentype issue. Maybe a fix for the jenkins builder?
    Signed-off-by: mcsnetworks <mike@mcsnetworks.com>

--- src/main/java/com/almuramc/digilock/gui/KeyHandler.java
+++ src/main/java/com/almuramc/digilock/gui/KeyHandler.java
@@ -32,7 +32,7 @@ public class KeyHandler implements BindingExecutionDelegate {
 	public void keyReleased(KeyBindingEvent event) {
 		SpoutPlayer sPlayer = event.getPlayer();
 		ScreenType screentype = event.getScreenType();
-		if (screentype == ScreenType.CUSTOM_SCREEN  || screentype == ScreenType.CHAT_SCREEN) {
+		if (screentype != ScreenType.GAME_SCREEN) {
 			return;
 		}
 		Block targetblock = (Block) sPlayer.getTargetBlock(null, 4);
